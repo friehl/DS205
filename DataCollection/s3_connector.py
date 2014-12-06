4a5723931a6cd10f974d0143f936cde2b03ac5c3
friehl <fletcher.riehl@gmail.com>
2014-12-06 13:32:32 -0800

    remove files

--- DataCollection/s3_connector.py
+++
@@ -1,8 +0,0 @@
-import boto
-#from boto.s3.connection import S3Connection
-from boto.s3.connection import OrdinaryCallingFormat
-
-conn = boto.connect_s3(calling_format=OrdinaryCallingFormat())
-bucket = conn.get_bucket('s3://ds205yelpdata/yelpdataset')
-for key in bucket.list():
-	print key.name.enconde('utf-8') 
